03324b1e69f7d89a91d3c709baa1402abfe757a5
Brian Kozub <ticet11@gmail.com>
2020-06-11 22:36:36 -0600

    How to Build a Form Input Component in React

--- src/components/auth/SignInForm.js
+++ src/components/auth/SignInForm.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
 
+import { FormInput } from "../FormFields";
+
 class SignInForm extends Component {
     render() {
         const { className } = this.props;
         return (
             <form className={`${className} sign-in-form`}>
-                Sign In
+                <Field
+                    className="sign-in-form__email"
+                    name='email'
+                    component={FormInput}
+                    type='email'
+                    title='E-mail'
+                    placeholder='E-mail'
+                />
+                <Field
+                    className="sign-in-form__password"
+                    name='password'
+                    component={FormInput}
+                    type='password'
+                    title='Password'
+                    placeholder='Password'
+                />
             </form>
         );
     }
