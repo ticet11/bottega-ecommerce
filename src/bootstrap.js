cfbed4f1cea2f46ec9622ee2ffe669eefa450c85
Brian Kozub <ticet11@gmail.com>
2020-06-11 20:53:02 -0600

    Sign In and Sign Up Component Setup

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,27 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter } from "react-router-dom";
+import { Router, Switch, Route } from "react-router-dom";
 import reducers from "./reducers";
 
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
 import Layout from "./components/Layout";
+import Signin from './components/auth/Signin';
+import Signup from './components/auth/Signup';
+import history from "./history";
 
 import "./style/main.scss";
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 function main() {
-  ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
-      <BrowserRouter>
-        <Layout>
-         
-        </Layout>
-      </BrowserRouter>
-    </Provider>,
-    document.querySelector(".app-wrapper")
-  );
+    ReactDOM.render(
+        <Provider store={createStoreWithMiddleware(reducers)}>
+            <Router history={history}>
+                <Layout>
+                    <Switch>
+                        <Route path="/" exact component={Signin} />
+                    </Switch>
+                </Layout>
+            </Router>
+        </Provider>,
+        document.querySelector(".app-wrapper")
+    );
 }
 
 document.addEventListener("DOMContentLoaded", main);
